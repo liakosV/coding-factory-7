e0c2313a6dc16b08f98ba021fbe11fb3ad80db09
LiakosV <billiak@hotmail.com>
2024-12-10 20:09:47 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("okeeeee");
+        int sum = 10;
+        System.out.println(sum);
+    }
+/*cfdhdfjhngf*/
+}
