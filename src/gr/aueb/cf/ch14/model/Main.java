5db2e4262f466c99dfcc143add21a6db24ff9025
LiakosV <billiak@hotmail.com>
2025-01-15 18:23:45 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch14/model/Main.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch14.model;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher(1, "Athanasios", "Androutsos");
+        Teacher teacher2 = new Teacher(2, "Vasilis", "Liakos");
+        Teacher teacher3 = new Teacher(3, "Kwstas", "Lokos");
+    }
+}
