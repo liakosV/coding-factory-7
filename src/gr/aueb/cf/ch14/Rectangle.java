0d457133480861771ee936b29d474335fe8c1a5a
LiakosV <billiak@hotmail.com>
2025-01-16 19:21:29 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch14/Rectangle.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.ch14;
+
+public class Rectangle extends Shape{
+
+//    public Rectangle() {  //Μας το δινει η Java απο μονη της.
+//
+//    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a rectangle");
+    }
+}
