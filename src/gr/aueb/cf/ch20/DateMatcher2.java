ffd2e803035d63404cbf4ea0d6fac89c18e8e1d0
LiakosV <billiak@hotmail.com>
2025-02-10 21:01:48 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch20/DateMatcher2.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf.ch20;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DateMatcher2 {
+
+    public static void main(String[] args) {
+        String date = "21/06/2025 07/08/2024";         //dd/mm/yyyy
+
+        Pattern pattern = Pattern.compile("(\\d{2})/(\\d{2})/(\\d{4})");
+        Matcher matcher = pattern.matcher(date);
+
+        while (matcher.find()) {
+            String grDate = "";
+            for (int i = 1; i <= matcher.groupCount(); i++) {
+                if (i != matcher.groupCount()) {
+                    System.out.print(matcher.group(i) + "/");
+                } else {
+                    System.out.print(matcher.group(i) + "");
+                }
+
+//                grDate += matcher.group(i) + "/";
+            }
+            System.out.println();
+        }
+    }
+}
