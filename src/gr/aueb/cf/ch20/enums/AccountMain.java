7cb23683bdc3c5f13cd84cd11ce5bbc64a5cee0a
LiakosV <billiak@hotmail.com>
2025-02-11 19:19:43 +0200

    initial commit

---
+++ src/gr/aueb/cf/ch20/enums/AccountMain.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch20.enums;
+
+public class AccountMain {
+
+    public static void main(String[] args) {
+        AccountType accountType = AccountType.CURRENT;
+
+        //enum -> type
+        String strAccountType = accountType.name();
+        System.out.println(strAccountType);
+
+        //enum -> string
+        AccountType accountType1 = AccountType.valueOf(strAccountType);
+
+        for (AccountType value : AccountType.values()) {
+            System.out.print(value.name() + " ");         // string
+            System.out.println(value.ordinal());            // ordinal number
+        }
+
+    }
+}
---
+++ src/gr/aueb/cf/ch20/enums/AccountType.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf.ch20.enums;
+
+public enum AccountType {
+    DEPOSIT, // ordinal 0
+    SAVINGS, // ordinal 1
+    CURRENT  // ordinal 2
+}
