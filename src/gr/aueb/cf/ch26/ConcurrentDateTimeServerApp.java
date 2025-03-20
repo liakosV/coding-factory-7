378308a6ac8fe9eb8f984f6ae881ae25fd99c063
LiakosV <billiak@hotmail.com>
2025-03-20 14:36:28 +0200

    feat: ch26 echo server and DateTime server created

---
+++ src/gr/aueb/cf/ch26/ConcurrentDateTimeServerApp.java
@@ -0,0 +1,27 @@
+package gr.aueb.cf.ch26;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ConcurrentDateTimeServerApp {
+
+    private static final int PORT = 13;
+
+    public static void main(String[] args) {
+
+        try (ServerSocket serverSocket = new ServerSocket()) {
+            serverSocket.bind(new InetSocketAddress("127.0.0.1", PORT));
+            System.out.println("DateTime Server has started...");
+
+            for (;;) {
+                Socket connectedFd = serverSocket.accept();
+                Thread socketThread = new Thread(new ConcurrentDateTimeServer(connectedFd));
+                socketThread.start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
